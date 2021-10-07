9640dae8a9b0e06a09867390ddfba696cb768f45
Alvin Yuan <yuanyang.alvin@gmail.com>
2021-10-07 14:46:05 +0100

    completed LeapYear and HelloNumbers

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,13 +1,24 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Alvin Yuan
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    /** Check whether a year number is leap year.
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     private static void checkLeapYear(int year) {
-        if (isLeapYear(year)) {
+        if (isLeapYear(year) == true) {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
@@ -18,7 +29,7 @@ public class LeapYear {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+            System.out.println("e.g. java LeapYear 2000");
         }
         for (int i = 0; i < args.length; i++) {
             try {
